b13131c6c70d07033cc1b692e788ad03f86851bf
IyiadeShodimu <talk2iyiade@gmail.com>
2021-10-11 12:08:59 +0100

    Lab 1 Main JavaScript

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// the window load event handler
+function onLoad() {
+    var mainCanvas, mainContext;
+    // This function will initialise our variables
+    function initialiseCanvasContext() {
+        // Find the canvas element using its id attribute.
+        mainCanvas = document.getElementById('mainCanvas');
+        // if it couldn't be found 
+        if (!mainCanvas) {
+            // make a message box pop up with the error.
+            alert('Error: I cannot find the canvas element!');
+            return;
+        }
+         // Get the 2D canvas context.
+         mainContext = mainCanvas.getContext('2d');
+         if (!mainContext) {
+             alert('Error: failed to get context!');
+             return;
+         }      
+
+    }
+    initialiseCanvasContext();
+}
+
+
+window.addEventListener('load', onLoad, false);
