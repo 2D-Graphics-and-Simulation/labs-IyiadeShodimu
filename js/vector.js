759a34e4d24a80652cf2529865c86493e50cbf5d
IyiadeShodimu <talk2iyiade@gmail.com>
2021-10-25 11:32:21 +0100

    Passed Constructor Test by Adding x,y and z Variables

--- js/vector.js
+++ js/vector.js
@@ -1,5 +1,25 @@
-class vector {
+class Vector {
     constructor(pX, pY, pZ) {
-        
+        this.setX(pX);
+        this.setY(pY);
+        this.setZ(pZ);
+    }
+    getX() {
+        return this.mX;
+    }
+    setX(pX) {
+        this.mX = pX;
+    }
+    getY() {
+        return this.mY;
+    }
+    setY(pY) {
+        this.mY = pY
+    }
+    getZ() {
+        return this.mZ;
+    }
+    setZ(pZ) {
+        this.mZ = pZ;
     }
 }
