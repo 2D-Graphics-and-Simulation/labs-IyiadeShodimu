7b7390ab0ae94150fda723b9d018d91bb587bfab
IyiadeShodimu <talk2iyiade@gmail.com>
2021-12-15 21:09:16 +0000

    Have made house JS file

---
+++ js/house.js
@@ -0,0 +1,15 @@
+class House {
+    constructor(pPosition){
+         this.setPosition(pPosition);
+    }
+
+getPosition() {
+    return this.mPosition;
+}
+setPosition() {
+    this.mPosition;
+}
+draw(pContext) {
+
+}
+}
