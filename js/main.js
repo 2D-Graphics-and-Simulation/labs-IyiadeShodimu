f5fc271f29c65b3483fdd6aae9fdd6cdd8d0d7e7
IyiadeShodimu <talk2iyiade@gmail.com>
2021-12-15 20:30:09 +0000

    I have drawn a house

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // the window load event handler
 function onLoad() {
-    var mainCanvas, mainContext, textPosition;
+    var mainCanvas, mainContext;
     // This function will initialise our variables
     function initialiseCanvasContext() {
         // Find the canvas element using its id attribute.
@@ -17,21 +17,76 @@ function onLoad() {
              alert('Error: failed to get context!');
              return;
          }
-         textPosition = new Vector(150, 100, 1);
+        
 
     }
     // this function will actually draw on the canvas
     function draw() {
         // set the draw fill style colour to black
-        mainContext.fillStyle = "#000000";
+        mainContext.fillStyle = "#cccccc";
         // fill the canvas with black
         mainContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
-        // choose a font for our message
-        mainContext.font = "40pt Century Gothic";
-        // set the draw fill colour to white
-        mainContext.fillStyle = "#ffffff";
-        // draw the text at the specified position
-        mainContext.fillText("Hello World!", textPosition.getX(), textPosition.getY());
+        mainContext.lineWidth = 5;
+        mainContext.lineJoin = 'round' ;     
+        drawWall(mainContext);  
+        drawRoof(mainContext);     
+        drawDoor(mainContext);
+        drawLeftWindow(mainContext);
+        drawRightWindow(mainContext);
+    }
+    function drawWall(pContext) {
+        pContext.beginPath();
+        pContext.fillStyle = "#ffffff";
+        pContext.moveTo(200, 200);
+        pContext.lineTo(400, 200);
+        pContext.lineTo(400, 300);
+        pContext.lineTo(200, 300);
+        pContext.closePath();
+        pContext.fill();
+        pContext.stroke();
+    }
+    function drawRoof(pContext) {
+        pContext.beginPath();
+        pContext.fillStyle = "#ff0000";
+        pContext.moveTo(200, 200);
+        pContext.lineTo(300, 100);
+        pContext.lineTo(400, 200);
+        pContext.closePath();
+        pContext.fill();
+        pContext.stroke();
+    }
+    function drawDoor(pContext) {
+        pContext.beginPath();
+        pContext.fillStyle = "#ffffff";
+        pContext.moveTo(275, 300);
+        pContext.lineTo(275, 225);
+        pContext.lineTo(325, 225);
+        pContext.lineTo(325, 300);
+        pContext.closePath();
+        pContext.fill();
+        pContext.stroke();
+    }
+    function drawLeftWindow(pContext) {
+        pContext.beginPath();
+        pContext.fillStyle = "#0000ff";
+        pContext.moveTo(220, 275);
+        pContext.lineTo(220, 225);
+        pContext.lineTo(250, 225);
+        pContext.lineTo(250, 275);
+        pContext.closePath();
+        pContext.fill();
+        pContext.stroke();
+    }
+    function drawRightWindow(pContext) {
+        pContext.beginPath();
+        pContext.fillStyle = "#0000ff";
+        pContext.moveTo(350, 275);
+        pContext.lineTo(350, 225);
+        pContext.lineTo(380, 225);
+        pContext.lineTo(380, 275);
+        pContext.closePath();
+        pContext.fill();
+        pContext.stroke();
     }
     initialiseCanvasContext();
     draw();
