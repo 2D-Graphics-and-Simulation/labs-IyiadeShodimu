7d4204c8b9d9b0d3a0f113d1af32b6bf73b2c480
Iyiade Shodimu <soulkid2k@gmail.com>
2023-06-28 14:14:47 +0100

    All Vector Tests passed

--- js/vector.js
+++ js/vector.js
@@ -87,6 +87,13 @@ class Vector {
     limitTo = function (Scalar) {
         this.normalise();
         this.multiply(Scalar);
+        if(this.magnitude() > Scalar){
+           var newX = (this.getX()/this.magnitude()) * Scalar;
+           var newY = (this.getY()/this.magnitude()) * Scalar;
+           var newZ = (this.getZ()/this.magnitude()) * Scalar;
+
+            return new Vector(newX, newY, newZ);
+        }
         return new Vector(this.getX(), this.getY(), this.getZ());
     };
           
@@ -96,14 +103,34 @@ class Vector {
 
         return DotProduct;
     }
+
     interpolate(pVector, pScalar) {
-        var firstVector = new Vector(this.getX(), this.getY(), this.getZ());
-        var newVector = new Vector(pVector.getX(), pVector.getY(), pVector.getZ());
-        var interpolatedVector = new Vector(newVector.subtract(firstVector));
+        var interpolX = this.getX() + (pVector.getX() - this.getX()) * pScalar;
+        var interpolY = this.getY() + (pVector.getY() - this.getY()) * pScalar;
+        var interpolZ = this.getZ() + (pVector.getZ() - this.getZ()) * pScalar;
 
-        var finalVector = firstVector + (interpolatedVector.multiply(pScalar));
+        return new Vector(interpolX, interpolY, interpolZ);
+    }
 
-        return finalVector;
+    rotate(Scalar) {
+        var cosAngle = Math.cos(Scalar);
+        var sinAngle = Math.sin(Scalar);
 
+        var rotateX = ((this.getX() * cosAngle) - (this.getY() * sinAngle));
+        var rotateY = ((this.getX() * sinAngle) + (this.getY() * cosAngle));
+        var rotateZ = this.getZ();
+        
+        
+        return new Vector(rotateX, rotateY, rotateZ);
+    }
+
+    angleBetween(pVector) {
+        var dotVector = this.dotProduct(pVector);
+        var mag1 = this.magnitude();
+        var mag2 = pVector.magnitude();
+
+        var angleBet = Math.acos(dotVector/(mag1 * mag2));
+
+        return angleBet;
     }
 }
